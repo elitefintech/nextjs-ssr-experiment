fe3ad54ee752aeb734230cad4d502acce4dfbd0b
Dave Maple <dave.maple@brandedholdings.com>
2021-12-21 21:52:08 -0800

    removing dist

--- dist/Variant.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export interface Props {
-    name: string;
-    onClick?: (event: React.MouseEvent<HTMLElement>, variantName: string) => void;
-    onRunVariant?: (variantName: string) => void;
-    [x: string]: any;
-}
-export declare function Variant(props: Props): JSX.Element;
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ExperimentProvider, ExperimentContext } from './ExperimentProvider';
-import { Experiment } from './Experiment';
-import { Variant } from './Variant';
-export { ExperimentProvider, ExperimentContext, Experiment, Variant };
